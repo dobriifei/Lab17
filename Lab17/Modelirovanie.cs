b47f3ad6bd4c1e420255040fe7bb7da5d88ca6e3
vermei2@outlook.com <vermei2@outlook.com>
2022-01-24 23:06:35 +0300

    Добавьте файлы проекта.

---
+++ Lab17/Modelirovanie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    class Modelirovanie<T>
+    {
+        public T Num { get; set; }
+        public string Fio { get; set; }
+        public int Balance { get; set; }
+
+        public Modelirovanie(T num, string fio, int balance)
+        {
+            Num = num;
+            Fio = fio;
+            Balance = balance;
+        }
+
+        public string GetInfo()
+        {
+            return $"{Num} {Fio} {Balance}";
+        }
+
+
+    }
+}
---
+++ Lab17/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер счета");
+            int numInt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ФИО");
+            string fio = Console.ReadLine();
+            Console.WriteLine("Укажите баланс");
+            int balance = Convert.ToInt32(Console.ReadLine());
+            Modelirovanie<int> modelirovanie1 = new Modelirovanie<int>(numInt, fio, balance);
+            Console.WriteLine(modelirovanie1.GetInfo());
+
+            Console.WriteLine("Введите строковый номер счета");
+            string numString = Console.ReadLine();
+            Console.WriteLine("Введите ФИО");
+            fio = Console.ReadLine();
+            Console.WriteLine("Укажите баланс");
+            balance = Convert.ToInt32(Console.ReadLine());
+            Modelirovanie<string> modelirovanie2 = new Modelirovanie<string>(numString, fio, balance);
+            Console.WriteLine(modelirovanie2.GetInfo());
+            Console.ReadKey();
+        }
+    }
+}
